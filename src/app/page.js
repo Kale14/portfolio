b74c96087fa8e048c5497484e878525391871f7a
Kale14 <72563746+Kale14@users.noreply.github.com>
2025-01-30 16:38:47 +0100

    Undo Changes for mobile

--- src/app/page.js
+++ src/app/page.js
@@ -6,12 +6,12 @@ import ProjectSectionDone from "@/app/components/ProjectSectionDone";
 
 export default function Page() {
     return (
-        <main className="max-w-[1200px] w-full mx-auto px-4 sm:px-6 md:px-8">
+        <main>
             <AboutSection />
             <ProjectsSectionGoing />
             <ProjectSectionDone />
             <TimelineSection />
-            <footer className="text-black py-4 mt-14 text-center">
+            <footer className= "text-black py-4 mt-14 text-center">
                 <p className="text-sm">
                     © 2025 Khalil Hasan. Alle Rechte vorbehalten.
                 </p>
