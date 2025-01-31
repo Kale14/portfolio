34e10f7acc801e7e0963832aa533bba7eb8b302e
Kale14 <72563746+Kale14@users.noreply.github.com>
2025-01-31 18:17:45 +0100

    change date value of projects

--- src/app/components/projectsData.js
+++ src/app/components/projectsData.js
@@ -2,9 +2,26 @@
 const projects = [
 
 
+    //Part Hackathon1
+    { title: "Sensoria ",
+        date: "2025",
+        duration: "3 Monate",
+        company: "Google",
+        status: "In Arbeit",
+        description: "Prototyp für Barrierefreiheit beim Baseball.",
+        details: `Der <span class="highlight">Prototype</span> wurde im Rahmen des <span class="highlight">Google Cloud x MLB™ Hackathons – Building with Gemini Models</span> entwickelt, um <span class="highlight">blinden Menschen</span> ein <span class="highlight">barrierefreies Baseballerlebnis</span> zu ermöglichen. Der Prototyp kombiniert <span class="highlight">Vibrationsmuster</span> zur <span class="highlight">Echtzeit-Übertragung</span> von <span class="highlight">Spielereignissen</span> mit einer <span class="highlight">verständlichen Audiozusammenfassung</span> der <span class="highlight">Highlights</span> nach Spielende.  
+
+Für die Umsetzung wurden <span class="highlight">Echtzeit-Spielereignisse</span> wie <span class="highlight">Würfe</span>, <span class="highlight">Treffer</span> und <span class="highlight">Spielerbewegungen</span> durch <span class="highlight">Google AI-Tools</span> analysiert und in <span class="highlight">priorisierte Kategorien</span> eingeteilt. Wichtige Ereignisse wurden durch <span class="highlight">spezifische Vibrationsmuster</span> vermittelt – von <span class="highlight">kurzen Impulsen</span> für einfache Aktionen bis hin zu <span class="highlight">intensiveren Signalen</span> für bedeutende Momente wie Home Runs. Eine <span class="highlight">NLP-basierte Audiozusammenfassung</span> stellte sicher, dass <span class="highlight">Nutzer</span> nach dem Spiel eine <span class="highlight">verständliche Rückschau</span> der wichtigsten <span class="highlight">Szenen</span> erhielten.  
+
+Die <span class="highlight">Entwicklung</span> basierte auf einer <span class="highlight">Nutzerbefragung</span>, um die <span class="highlight">Anforderungen blinder Menschen</span> gezielt zu berücksichtigen. Anschließende <span class="highlight">Usability-Tests</span> überprüften die <span class="highlight">Intuitivität der Vibrationsmuster</span> und die <span class="highlight">Verständlichkeit der Audiodaten</span>, wobei <span class="highlight">iteratives Feedback</span> zur <span class="highlight">Optimierung des Systems</span> genutzt wurde.  
+                  `,
+        image: "/baseball.webp",
+        imagep: "/highway.png" },
+
+
     //Part Bachelorarbeit
     { title: "Bachelorarbeit",
-        date: "10/2024",
+        date: "2024",
         duration: "3 Monate",
         company: "HTW",
         status: "Abgeschlossen",
@@ -21,7 +38,7 @@ Beim <span class="highlight">Benchmarking</span> wurden <span class="highlight">
 
     //Part Hackathon2
     { title: "UrbanGuide",
-        date: "05/2024",
+        date: "2024",
         duration: "2 Monate",
         company: "Google",
         status: "Abgeschlossen",
@@ -39,7 +56,7 @@ Die <span class="highlight">Architektur</span> der App basierte auf einer skalie
 
     //Digital Twin
     { title: "Digital Twin",
-        date: "03/2023",
+        date: "2023",
         duration: "8 Monate",
         company: "PwC",
         status: "Abgeschlossen",
@@ -57,7 +74,7 @@ Dabei erfolgte eine Analyse der Stakeholder- Anforderungen, um Ziele zu definier
 
     //Klassifizierung
     { title: "Brückenschäden",
-        date: "03/2023",
+        date: "2023",
         duration: "2 Monate",
         company: "PWC",
         status: "Abgeschlossen",
@@ -74,7 +91,7 @@ Eine <span class="highlight">Pipeline</span>, implementiert mit <span class="hig
 
     //Überwachung
     { title: "Überwachungslösung",
-        date: "03/2023",
+        date: "2023",
         duration: "6 Monate",
         company: "PWC",
         status: "Abgeschlossen",
@@ -89,7 +106,7 @@ Das Projekt wurde in mehreren Schritten realisiert: Zunächst wurden DSGVO-konfo
 
     //Gestenstreuerung
     { title: "Gestensteuerung",
-        date: "10/2022",
+        date: "2022",
         duration: "1 Monate",
         company: "Privat",
         status: "Abgeschlossen",
@@ -106,7 +123,7 @@ Das Modell basierte auf einem <span class="highlight">Convolutional Neural Netwo
 
     //WhoIsDown
     { title: "WhoIsDown",
-        date: "07/2021",
+        date: "2021",
         duration: "6 Monate",
         company: "HTW",
         status: "Abgeschlossen",
