e680675c231d093882fe9d283160e2fa0bfc0cdd
Kale14 <72563746+Kale14@users.noreply.github.com>
2025-02-06 15:29:26 +0100

    add Analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,10 +1,12 @@
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react"
 
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
         <body className="relative bg-[#0a192f] text-white">
-        {children} {/* Inhalte der Seite */}
+            {children} {/* Inhalte der Seite */}
+            <Analytics />
         </body>
         </html>
     );
